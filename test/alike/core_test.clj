f9676cddcaa1f05c0a7cb2d1f3929397226a19e4
Ivan Grishaev <ivan@grishaev.me>
2026-04-30 18:21:09 +0300

    add tests

--- test/alike/core_test.clj
+++ test/alike/core_test.clj
@@ -1,7 +1,32 @@
 (ns alike.core-test
-  (:require [clojure.test :refer :all]
-            [alike.core :refer :all]))
+  (:require
+   [clojure.test :refer [are is deftest testing]]
+   [alike.core :as alike]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-equality
+  (are [a b] (alike/match a b)
+    1 1
+    1 1.0
+    1.0 1
+    "a" "a"
+    true true
+    false false
+    nil nil
+    :foo :foo
+    'aaa 'bb
+    + +
+    \a \a
+    (/ 3 2) (/ 3 2)))
+
+(deftest test-functions
+  (are [a b] (alike/match a b)
+    int? 1
+    float? 1.0
+    nil? nil
+    string? "test"
+    map? {}
+    vector? []
+    list? ()
+    set? #{}
+    empty? #{}
+    ifn? +))
